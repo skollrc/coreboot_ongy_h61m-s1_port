fda8b53f1419d4b8c15213bc38d488f96ea19489
skollrc <35557003+skollrc@users.noreply.github.com>
2020-04-27 00:25:31 +0200

    remove useless diplay ports

--- ongy/h61m-s1/gma-mainboard.ads
+++ ongy/h61m-s1/gma-mainboard.ads
@@ -9,16 +9,9 @@ use HW.GFX.GMA.Display_Probing;
 
 private package GMA.Mainboard is
 
-   -- FIXME: check this
-   ports : constant Port_List :=
-     (DP1,
-      DP2,
-      DP3,
-      HDMI1,
-      HDMI2,
-      HDMI3,
+  ports : constant Port_List :=
+     (HDMI1,
       Analog,
-      LVDS,
-      eDP);
+      others => Disabled);
 
 end GMA.Mainboard;
