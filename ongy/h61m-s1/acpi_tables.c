7f459a839bad264d64cd6ada0ef886ef45874de9
skollrc <35557003+skollrc@users.noreply.github.com>
2020-04-27 01:18:39 +0200

    fix lid issue

--- ongy/h61m-s1/acpi_tables.c
+++ ongy/h61m-s1/acpi_tables.c
@@ -1,9 +1,16 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-/* This file is part of the coreboot project. */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #include <southbridge/intel/bd82x6x/nvs.h>
 
-/* FIXME: check this function.  */
 void acpi_create_gnvs(global_nvs_t *gnvs)
 {
 	/* Disable USB ports in S3 by default */
@@ -13,10 +20,4 @@ void acpi_create_gnvs(global_nvs_t *gnvs)
 	/* Disable USB ports in S5 by default */
 	gnvs->s5u0 = 0;
 	gnvs->s5u1 = 0;
-
-	/* The lid is open by default. */
-	gnvs->lids = 1;
-
-	gnvs->tcrt = 100;
-	gnvs->tpsv = 90;
 }
