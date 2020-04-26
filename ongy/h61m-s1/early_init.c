29a45cafe4e43a794379b299237b40eaca519531
skollrc <35557003+skollrc@users.noreply.github.com>
2020-04-27 01:07:32 +0200

    fix get SPD function

--- ongy/h61m-s1/early_init.c
+++ ongy/h61m-s1/early_init.c
@@ -40,11 +40,8 @@ void bootblock_mainboard_early_init(void)
 	pci_write_config16(PCI_DEV(0, 0x1f, 0), 0x80, 0x0000);
 }
 
-/* FIXME: Put proper SPD map here. */
 void mainboard_get_spd(spd_raw_data *spd, bool id_only)
 {
 	read_spd(&spd[0], 0x50, id_only);
-	read_spd(&spd[1], 0x51, id_only);
 	read_spd(&spd[2], 0x52, id_only);
-	read_spd(&spd[3], 0x53, id_only);
 }
