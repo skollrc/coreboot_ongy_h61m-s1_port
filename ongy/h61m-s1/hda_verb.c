cd08f7bb26f680a9b14397babd6c107990551ae0
Skoll RC <35557003+skollrc@users.noreply.github.com>
2020-04-28 21:12:07 +0200

    Update hda_verb.c

--- ongy/h61m-s1/hda_verb.c
+++ ongy/h61m-s1/hda_verb.c
@@ -1,5 +1,5 @@
 /*
-  * This program is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; version 2 of
  * the License.
@@ -13,9 +13,9 @@
 #include <device/azalia_device.h>
 
 const u32 cim_verb_data[] = {
-	0x10ec0662,	/* Codec Vendor / Device ID: Realtek */
-	0x10ec0000,	/* Subsystem ID */
-	12,		/* Number of 4 dword sets */
+	0x10ec0662,	/* Codec Vendor / Device ID: Realtek*/
+	0x10ec0000,	/* Subsystem ID*/
+	12,		/* Number of 4 dword sets*/
 	AZALIA_SUBVENDOR(2, 0x10ec0000),
 	AZALIA_PIN_CFG(2, 0x12, 0x40130000),
 	AZALIA_PIN_CFG(2, 0x14, 0x01014010),
@@ -29,14 +29,13 @@ const u32 cim_verb_data[] = {
 	AZALIA_PIN_CFG(2, 0x1d, 0x4044c601),
 	AZALIA_PIN_CFG(2, 0x1e, 0x01441130),
 
-	0x80862805,	/* Codec Vendor / Device ID: Intel */
-	0x80860101,	/* Subsystem ID */
-	4,		/* Number of 4 dword sets */
+	0x80862805,	/* Codec Vendor / Device ID: Intel*/
+	0x80860101,	/* Subsystem ID*/
+	4,		/* Number of 4 dword sets*/
 	AZALIA_SUBVENDOR(3, 0x80860101),
 	AZALIA_PIN_CFG(3, 0x05, 0x18560010),
 	AZALIA_PIN_CFG(3, 0x06, 0x18560020),
 	AZALIA_PIN_CFG(3, 0x07, 0x18560030),
-
 };
 
 const u32 pc_beep_verbs[0] = {};
